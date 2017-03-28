e269b664830ff7fddef615c883465e984641ebe8
4015a1 <4015a1@icaco.ece.uprm.edu>
2017-03-28 18:41:40 -0400

    Added legend

--- [Project1] La-La-BOOMLAND/src/Main.java
+++ [Project1] La-La-BOOMLAND/src/Main.java
@@ -1,4 +1,6 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,15 +14,38 @@ public class Main {
 
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
 		myFrame.addMouseListener(myMouseAdapter);
-
+		
+	
 		myFrame.setVisible(true);
+		
 		
 		Boomland.newGame();
 		
 		
+		String Title = "Leyenda" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>"
+		+ "Color1 = 1" + "<br>";
+	
+		JLabel Legend = new JLabel ("<html><div style='text-align: center;'>" + Title + "</div></html>");
+		
+		
+		JFrame myLegend = new JFrame("La-La-Legend");
+		myLegend.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myLegend.setLocation(800, 150);
+		myLegend.setSize(250, 310);
+		myLegend.add(Legend);
+		myLegend.setVisible(true);
+	
+		
+		
 	}
-	
-	
+
 	
 	
 	
